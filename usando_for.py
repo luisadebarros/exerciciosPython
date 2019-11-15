dae383c2489275e119b7ac374cfacdfc0acc39bc
Matheus Ferreira <matheus.domingues3@etec.sp.gov.br>
2019-11-14 21:30:23 -0300

    Usando o for

---
+++ usando_for.py
@@ -0,0 +1,11 @@
+lista_nomes = ['bia', 'bicho', 'Joy', 'Yas', 'lu']
+
+#for nome in lista_nomes:
+#    print(nome)
+#else:
+#    print('')
+
+for item in range(0, 4):
+    print(item)
+    print(lista_nomes[item])
+
