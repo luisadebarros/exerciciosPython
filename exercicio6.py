4753f707b74944586d09c7e39b51b9da8f79a0d6
Matheus Ferreira <matheus.domingues3@etec.sp.gov.br>
2019-11-18 18:56:15 -0300

    Terminando exercicio 6

--- exercicio6.py
+++ exercicio6.py
@@ -3,3 +3,13 @@ peso = float(input(''))
 print('Insira a altura:')
 altura = float(input(''))
 
+imc = peso / (altura * altura)
+
+if imc < 18.5:
+    print('Peso: {}, Altura: {}, Imc: {}, você está abaixo do peso ideal'.format(peso, altura, imc))
+elif imc <= 24.9:
+    print('Peso: {}, Altura: {}, Imc: {}, peso normal'.format(peso, altura, imc))
+elif imc <= 30:
+    print('Peso: {}, Altura: {}, Imc: {}, acima do peso'.format(peso, altura, imc))
+elif imc > 30:
+    print('Peso: {}, Altura: {}, Imc: {}, você está obeso'.format(peso, altura, imc))
