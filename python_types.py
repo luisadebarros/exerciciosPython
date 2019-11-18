8bc55c39a691f0a6c3ee1460ba6fc6e85397c1ec
Matheus Ferreira <matheus.domingues3@etec.sp.gov.br>
2019-11-18 19:13:15 -0300

    Verificando python types

---
+++ python_types.py
@@ -0,0 +1,9 @@
+str = input('digite algo: ')
+
+print('Alpha: ', str.isalpha())
+print('Alfanum: ', str.isalnum())
+print('Numeric: ', str.isnumeric())
+print('Uppercase: ', str.isupper())
+print('Lowercase: ', str.islower())
+print('Capitalize: ', str.istitle())
+print('Only with spaces: ', str.isspace())
