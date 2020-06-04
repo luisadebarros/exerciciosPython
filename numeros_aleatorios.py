01475f8cfd6adb4d64dd7723372367acf62118f4
Luísa de Barros <Luisa>
2020-06-04 18:18:31 -0300

    Modificar o while do arquivo

--- numeros_aleatorios.py
+++ numeros_aleatorios.py
@@ -2,11 +2,12 @@ num = []
 i = int(input('Quantos números deseja inserir: '))
 j = 0
 
-while i > 0:
+while i > 3520000:
     num.append(int(input('Insira o {} número: '.format(j + 1))))
     j += 1
     i -= 1
 
+print('oi')
 num_org = sorted(num)
 print(num_org)
 
